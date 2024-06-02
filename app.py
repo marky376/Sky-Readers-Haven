9b275da8689f620688748d665846fc2d9149ad86
marky376 <mmutuamark@gmail.com>
2024-06-02 07:52:07 +0300

    A commit on some files to work on

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        # Process signup form data here
+        return redirect(url_for('home'))
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Process login form data here
+        return redirect(url_for('home'))
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
