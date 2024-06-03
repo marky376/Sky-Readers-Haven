68bc612d5bb6c4cfaf483d519ef3e64632a1813c
marky376 <mmutuamark@gmail.com>
2024-06-03 05:47:35 +0300

    Added user preferences table - Created a new table to store user preferences such as theme settings and notification preferences. - Added columns for theme choice, email notifications, and push notifications

--- app.py
+++ app.py
@@ -1,6 +1,44 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, flash, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, Email
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
+import bcrypt
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'your_email@gmail.com'
+app.config['MAIL_PASSWORD'] = 'your_password'
+
+mail = Mail(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    confirmed = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}')"
+
+class SignupForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
 @app.route('/')
 def home():
@@ -8,17 +46,40 @@ def home():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    if request.method == 'POST':
-        # Process signup form data here
-        return redirect(url_for('home'))
-    return render_template('signup.html')
+    form = SignupForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(name=form.name.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
 
-@app.route('/login', methods=['GET', 'POST'])
+        token = s.dumps(form.email.data, salt='email-confirm')
+        msg = Message('Confirm Email', sender='noreply@demo.com', recipients=[form.email.data])
+        link = url_for('confirm_email', token=token, _external=True)
+        msg.body = 'Your link is {}'.format(link)
+        mail.send(msg)
+
+        flash('A confirmation email has been sent via email.', 'success')
+        return redirect(url_for('login'))
+        
+    return render_template('signup.html', form=form)
+
+@app.route('/login')
 def login():
-    if request.method == 'POST':
-        # Process login form data here
-        return redirect(url_for('home'))
     return render_template('login.html')
+
+@app.route('/confirm_email/<token>')
+def confirm_email(token):
+    try:
+        email = s.loads(token, salt='email-confirm', max_age=3600)
+    except SignatureExpired:
+        return '<h1>The token is expired!</h1>'
+    user = User.query.filter_by(email=email).first_or_404()
+    user.confirmed = True
+    db.session.add(user)
+    db.session.commit()
+    return '<h1>Your email is confirmed. Thanks!</h1>'
 
 if __name__ == "__main__":
     app.run(debug=True)
+s
